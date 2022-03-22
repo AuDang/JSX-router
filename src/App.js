fa4a0cc9b38657c8794a47890c14943c29e71598
Austin Dang <austindang415@gmail.com>
2022-03-22 12:56:17 -0700

    formatting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
+import {Route, Switch} from 'react-router-dom'
 import Home from './components/Home';
 import Stocks from './components/Stocks';
 import Movies from './components/Movies';
-import {Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
     <div className='main'>
       <h1>App Component</h1>
       <Switch>
-      <Route path='/'>
-        <Home />
-      </Route>
-      <Route path='/stocks'>
-         <Stocks/>
-       </Route>
-       <Route path='/movies'>
-         <Movies />
-       </Route>
+        <Route path='/'>
+          <Home />
+        </Route>
+        <Route path='/stocks'>
+          <Stocks/>
+        </Route>
+        <Route path='/movies'>
+          <Movies />
+        </Route>
       </Switch>
     </div>
   );
